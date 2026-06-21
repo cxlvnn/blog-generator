2c41a8fe43b923ff88c584bab7323ad14d81c3f3
cxlvnn <behruzturabekov19@gmail.com>
2026-06-22 01:03:46 +0500

    finally figured out how to work with different paths

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+HOST = "localhost"
+PORT = 8080
+
+with open("storage/blog-1.json", "r") as file:
+    blog_data = json.load(file)
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        response_txt: str
-        with open("resources/views/index.html", "r") as file:
-            response_txt = file.read()
-        _ = self.wfile.write(bytes(response_txt, "utf-8"))
+        path = self.path
+
+        match path:
+            case "/":
+                with open("resources/views/index.html", "r") as file:
+                    resp = file.read()
+                self.wfile.write(bytes(resp, "utf-8"))
+            case "/blog":
+                with open("resources/views/blog/show.html", "r") as file:
+                    resp = file.read()
+                    resp = resp.replace("^title^", blog_data["title"])
+                    resp = resp.replace("^content^", blog_data["content"])
+                    resp = resp.replace("^date^", blog_data["createdAt"])
+                self.wfile.write(bytes(resp, "utf-8"))
+            case _:
+                resp = b"Not Found"
+                self.wfile.write(resp)
 
 
 def run():
