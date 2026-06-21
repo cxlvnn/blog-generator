c70c90b64c3f18a171821f46ddb1e1d7145cbb3a
cxlvnn <behruzturabekov19@gmail.com>
2026-06-22 03:44:10 +0500

    showing individual blogs

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from dataclasses import dataclass
+
+
+@dataclass
+class Blog:
+    title = str
+    content = str
+    date = str
+
 
 HOST = "localhost"
 PORT = 8080
@@ -24,30 +33,55 @@ class Handler(BaseHTTPRequestHandler):
                 self.end_headers()
 
                 with open("resources/views/index.html", "r") as file:
-                    resp = file.read()
+                    app = file.read()
                     with open("resources/views/blog/index.html") as file:
                         blog_index = file.read()
-                        resp = resp.replace("^app^", blog_index)
+                        app = app.replace("^app^", blog_index)
                         blogs_template = "<ul>"
                         for blog in blogs:
                             blogs_template += f"""
                                                 <li>
-                                                <a href="/blog/{blog["id"]}">{blog["title"]}
+                                                <a href="/blogs/{blog["id"]}">{blog["title"]}
                                                 </a>
                                                 <p>{blog["date"]}</p>
                                                 </li>
                                                 """
                         blogs_template += "</ul>"
-                        resp = resp.replace("^blogs^", blogs_template)
+                        app = app.replace("^blogs^", blogs_template)
                     with open("resources/css/main.css") as file:
                         style = file.read()
-                        resp = resp.replace("^mystyle^", style)
-                self.wfile.write(bytes(resp, "utf-8"))
+                        app = app.replace("^mystyle^", style)
+                self.wfile.write(bytes(app, "utf-8"))
             case _:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+
                 path_list = path.split("/")
                 if len(path_list) == 3:
-                    id = path_list[2]
-                    self.wfile.write(bytes(id, "utf-8"))
+                    id = int
+                    if int(path_list[2]):
+                        id = int(path_list[2])
+                    title = ""
+                    content = ""
+                    date = ""
+                    for blog in blogs:
+                        if id == blog["id"]:
+                            title = blog["title"]
+                            content = blog["content"]
+                            date = blog["date"]
+                    with open("resources/views/index.html") as file:
+                        app = file.read()
+                        with open("resources/css/main.css") as css:
+                            style = css.read()
+                            app = app.replace("^mystyle^", style)
+                    with open("resources/views/blog/show.html") as file:
+                        blog_show = file.read()
+                        blog_show = blog_show.replace("^title^", title)
+                        blog_show = blog_show.replace("^content^", content)
+                        blog_show = blog_show.replace("^date^", date)
+                        app = app.replace("^app^", blog_show)
+                        self.wfile.write(bytes(app, "utf-8"))
 
 
 def run():
