610ee38e026f4f6be8c7df4bca8eafe0a9a26554
cxlvnn <behruzturabekov19@gmail.com>
2026-06-23 20:09:52 +0500

    admin.html file for the admin page and some styles to it

--- main.py
+++ main.py
@@ -95,6 +95,30 @@ class Handler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
+
+                with open("resources/views/index.html", "r") as file:
+                    app = file.read()
+                    with open("resources/views/auth/admin.html") as file:
+                        blog_index = file.read()
+                        app = app.replace("^app^", blog_index)
+                        blogs_template = "<ul>"
+                        for blog in blogs:
+                            blogs_template += f"""
+                                                <li>
+                                                <a href="/blogs/{blog["id"]}">{blog["title"]}
+                                                </a>
+                                                <div class="action-buttons-container">
+                                                <button class="edit-button">Edit</button>
+                                                <button class="delete-button">Delete</button>
+                                                </div>
+                                                </li>
+                                                """
+                        blogs_template += "</ul>"
+                        app = app.replace("^blogs^", blogs_template)
+                    with open("resources/css/main.css") as file:
+                        style = file.read()
+                        app = app.replace("^mystyle^", style)
+                self.wfile.write(bytes(app, "utf-8"))
             case _:
                 return_not_found(self)
 
