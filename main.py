57d2063ff10c1e6bff27e7cfe94afa24eda6e4c6
cxlvnn <behruzturabekov19@gmail.com>
2026-06-23 19:02:06 +0500

    better way of handling urls, not found cases etc

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from dataclasses import dataclass
-
-
-@dataclass
-class Blog:
-    title = str
-    content = str
-    date = str
-
 
 HOST = "localhost"
 PORT = 8080
+
+
+def return_not_found(handler: BaseHTTPRequestHandler):
+    handler.send_response(404)
+    handler.send_header("Content-type", "text/html")
+    handler.end_headers()
+
+    with open("resources/views/index.html", "r") as file:
+        app = file.read()
+        with open("resources/css/main.css", "r") as file:
+            app = app.replace("^mystyle^", file.read())
+        app = app.replace(
+            "^app^",
+            """
+                          <div>
+                          <h1>404 - Not Found</h1>
+                          </div>
+                          """,
+        )
+    handler.wfile.write(bytes(app, "utf-8"))
+
 
 with open("storage/blogs.json", "r") as file:
     blogs = json.load(file)
@@ -20,14 +32,14 @@ with open("storage/blogs.json", "r") as file:
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
-        path = self.path
+        path = [s for s in self.path.split("/") if s]
 
         match path:
-            case "/":
+            case []:
                 self.send_response(301)
                 self.send_header("Location", "/blogs")
                 self.end_headers()
-            case "/blogs":
+            case ["blogs"]:
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
@@ -52,36 +64,39 @@ class Handler(BaseHTTPRequestHandler):
                         style = file.read()
                         app = app.replace("^mystyle^", style)
                 self.wfile.write(bytes(app, "utf-8"))
-            case _:
+            case ["blogs", blog_id]:
+                try:
+                    blog_id = int(blog_id)
+                except ValueError:
+                    return_not_found(self)
+
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
 
-                path_list = path.split("/")
-                if len(path_list) == 3:
-                    id = int
-                    if int(path_list[2]):
-                        id = int(path_list[2])
-                    title = ""
-                    content = ""
-                    date = ""
-                    for blog in blogs:
-                        if id == blog["id"]:
-                            title = blog["title"]
-                            content = blog["content"]
-                            date = blog["date"]
-                    with open("resources/views/index.html") as file:
-                        app = file.read()
-                        with open("resources/css/main.css") as css:
-                            style = css.read()
-                            app = app.replace("^mystyle^", style)
-                    with open("resources/views/blog/show.html") as file:
-                        blog_show = file.read()
-                        blog_show = blog_show.replace("^title^", title)
-                        blog_show = blog_show.replace("^content^", content)
-                        blog_show = blog_show.replace("^date^", date)
+                for blog in blogs:
+                    if blog_id == blog["id"]:
+                        with open("resources/views/index.html", "r") as file:
+                            app = file.read()
+
+                        with open("resources/css/main.css", "r") as file:
+                            app = app.replace("^mystyle^", file.read())
+
+                        with open("resources/views/blog/show.html", "r") as file:
+                            blog_show = file.read()
+                            blog_show = blog_show.replace("^title^", blog["title"])
+                            blog_show = blog_show.replace("^content^", blog["content"])
+                            blog_show = blog_show.replace("^date^", blog["date"])
+
                         app = app.replace("^app^", blog_show)
                         self.wfile.write(bytes(app, "utf-8"))
+
+            case ["admin"]:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+            case _:
+                return_not_found(self)
 
 
 def run():
